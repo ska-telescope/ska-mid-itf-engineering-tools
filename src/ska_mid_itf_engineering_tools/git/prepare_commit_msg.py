1d3cee96b98942537a18f819b299e0ec290d2aa7
Pieter Jordaan <gitlab@jordaan.me>
2024-05-10 08:48:16 +0200

    AT-2008 - Added documentation and fixed linting.

--- src/ska_mid_itf_engineering_tools/git/prepare_commit_msg.py
+++ src/ska_mid_itf_engineering_tools/git/prepare_commit_msg.py
@@ -1,16 +1,32 @@
+"""Tooling to modify the commit message to include the Jira issue ID."""
+
 import argparse
 import logging
 import re
-from typing import List
+from typing import Optional
+
 import git
 
-class CommitMsgPreparer:
 
-    def __init__(self,
-                 excluded_branches=["develop", "master", "release/", "main"],
-                 included_origins=["git@gitlab.com:ska-telescope"],
-                 debug=False,
+class CommitMsgPreparer:
+    """CommitMsgPreparer is used to modify the commit message to include the Jira issue ID."""
+
+    def __init__(
+        self,
+        excluded_branches=["develop", "master", "release/", "main"],
+        included_origins=["git@gitlab.com:ska-telescope"],
+        debug=False,
     ) -> None:
+        """
+        Initialise the CommitMsgPreparer.
+
+        :param excluded_branches: branches to exclude when modifying commit messages.
+        :type excluded_branches: List[str]
+        :param included_origins: origins which are allowed when modifying commit messages.
+        :type included_origins: List[str]
+        :param debug: whether log messages should be printed or not.
+        :type debug: bool
+        """
         self.repo = git.Repo(".")
         self.excluded_branches = excluded_branches
         self.included_origins = included_origins
@@ -20,25 +36,62 @@ class CommitMsgPreparer:
         else:
             self.logger.setLevel(logging.WARNING)
 
-    def branch_jira_ticket_matches(self, branch: str) -> List[str]:
-        return re.findall("^[a-z]+-\d+", branch)
+    def jira_issue_id_from_branch(self, branch: str) -> Optional[str]:
+        """
+        Extract the Jira issue ID from the branch name.
 
-    def commit_contains_jira_ticket(self, commit: str) -> str:
-        return re.search("^[A-Z]+-\d+", commit) is not None
+        :param branch: the current branch.
+        :type branch: str
+        :return: The Jira issue ID if it is present, None otherwise.
+        :rtype: Optional[str]
+        """
+        matches = re.findall(r"^[a-z]+-\d+", branch)
+        if len(matches) == 0:
+            return None
+        return matches[0]
+
+    def commit_contains_jira_issue_id(self, commit: str) -> bool:
+        """
+        Determine whether the commit contains a Jira issue ID.
+
+        :param commit: The current commit message.
+        :type commit: str
+        :return: True if it contains a Jira issue ID, False otherwise.
+        :rtype: bool
+        """
+        return re.search(r"^[A-Z]+-\d+", commit) is not None
 
     def current_branch(self) -> str:
-        # git rev-parse --abbrev-ref HEAD
+        """
+        Get the current branch.
+
+        :return: the current branch.
+        :rtype: str
+        """
         return self.repo.active_branch.name
 
     def origin_url(self) -> str:
-        # git config --get remote.origin.url
+        """
+        Get the origin URL.
+
+        :return: the origin URL.
+        :rtype: str
+        """
         return self.repo.remote("origin").url
 
-    def prepare_msg(self, messageFile: str):
-        ## This is meant to use with the prepare-commit-msg hook on a global configuration.
-        ## This script uses a whitelist `IncludedOrigins` so it would only work for explicit sources,
-        ## and also uses a blacklist for branches to ignore `ExcludedBranches` 
-        ## that way it would only work for the projects and branches that you want
+    def prepare_msg(self, commit_message_file: str):
+        """
+        Prepends the Jira issue ID to the current commit message.
+
+        The Jira issue ID is extracted from the branch name.
+        If there is already a Jira issue ID in the commit message,
+        or the branch does not contain an issue ID, nothing is done.
+        The commit message is also not modified if the branch is an excluded branch
+        or if the origin does not match the whitelisted origins.
+
+        :param commit_message_file: the file containing the commit message to modify.
+        :type commit_message_file: str
+        """
         branch = self.current_branch()
         if any(branch.startswith(excluded) for excluded in self.excluded_branches):
             self.logger.debug("not modifying commit: on excluded branch: %s", branch)
@@ -46,30 +99,41 @@ class CommitMsgPreparer:
 
         origin = self.origin_url()
         if not any(origin.startswith(url) for url in self.included_origins):
-            self.logger.debug("not modifying commit: origin %s does not match included origins %s", origin, self.included_origins)
+            self.logger.debug(
+                "not modifying commit: origin %s does not match included origins %s",
+                origin,
+                self.included_origins,
+            )
             return
 
-        matches: List[str]  = self.branch_jira_ticket_matches(branch)
-        if len(matches) == 0:
+        jira_issue_id = self.jira_issue_id_from_branch(branch)
+        if jira_issue_id is None:
             self.logger.debug("not modifying commit: branch %s does not match", branch)
             return
 
-        with open(messageFile, encoding="utf-8", mode="r+") as commit_msg_file:
+        with open(commit_message_file, encoding="utf-8", mode="r+") as commit_msg_file:
             commit_msg = commit_msg_file.read()
-            if self.commit_contains_jira_ticket(commit_msg):
+            if self.commit_contains_jira_issue_id(commit_msg):
                 self.logger.debug("not modifying commit: commit %s does not match", commit_msg)
                 return
-            
-            modified_commit_msg = f"{matches[0].upper()} - {commit_msg}"
+
+            modified_commit_msg = f"{jira_issue_id.upper()} - {commit_msg}"
             commit_msg_file.seek(0)
             commit_msg_file.write(modified_commit_msg)
             self.logger.debug("modified commit message to:\n%s", modified_commit_msg)
         return
 
+
 def main() -> int:
+    """
+    Run the CommitMsgPreparer.
+
+    :return: The exit code: should always be 0 so that committing does not fail.
+    :rtype: int
+    """
     parser = argparse.ArgumentParser(
         prog="prepare_commit_msg",
-        description="Adds the active Jira Ticket ID to commit message if it is not present."
+        description="Adds the active Jira Ticket ID to commit message if it is not present.",
     )
     parser.add_argument("commit_file")
     args = parser.parse_args()
--- tests/unit/git/test_prepare_commit_msg.py
+++ tests/unit/git/test_prepare_commit_msg.py
@@ -1,32 +1,46 @@
-from dataclasses import dataclass
+"""Tests for prepare_commit_msg.py."""
+
 import os
 import pathlib
 import tempfile
-from typing import Any, Generator, Iterator
-from unittest.mock import MagicMock, PropertyMock
+from dataclasses import dataclass
+from typing import Any, Generator
+from unittest.mock import patch
 
 import pytest
-from unittest.mock import patch
-from ska_mid_itf_engineering_tools.git.prepare_commit_msg import CommitMsgPreparer
 
+from ska_mid_itf_engineering_tools.git.prepare_commit_msg import CommitMsgPreparer
 
 __testdata_dir = "tests/unit/git/testdata"
 
+
 @dataclass
 class CommitFixture:
+    """CommitFixture contains details about the commit being tested."""
+
     name: str
     path: str
     contents: str
     valid: bool
 
 
-@pytest.fixture(params=[
-    ("short_commit_without_jira_ticket.txt", True),
-    ("short_commit_with_jira_ticket.txt", False),
-    ("multiline_commit_without_jira_ticket.txt", True),
-    ("multiline_commit_with_jira_ticket.txt", False),
-])
-def commit_fixture(request) -> Generator[CommitFixture, None, None]:
+@pytest.fixture(
+    params=[
+        ("short_commit_without_jira_ticket.txt", True),
+        ("short_commit_with_jira_ticket.txt", False),
+        ("multiline_commit_without_jira_ticket.txt", True),
+        ("multiline_commit_with_jira_ticket.txt", False),
+    ]
+)
+def commit_fixture(request: Any) -> Generator[CommitFixture, None, None]:
+    """
+    Yield a fixture containing details about the commit.
+
+    :param request: pytest fixture containing commit params.
+    :type request: Any
+    :yield: a fixture containing details about the commit.
+    :rtype: Generator[CommitFixture, None, None]
+    """
     commit_file_name: str = request.param[0]
     commit_valid: bool = request.param[1]
     with open(pathlib.Path(__testdata_dir, commit_file_name), encoding="utf-8") as f:
@@ -39,25 +53,48 @@ def commit_fixture(request) -> Generator[CommitFixture, None, None]:
     os.remove(temp_path)
     os.rmdir(temp_dir)
 
+
 @dataclass
 class BranchFixture:
+    """BranchFixture contains details about the branch being tested."""
+
     name: str
     jira_issue: str = ""
     valid: bool = False
 
-@pytest.fixture(params=[
-    BranchFixture("at-1234-test-branch", valid=True, jira_issue="AT-1234"), 
-    BranchFixture("map-1", valid=True, jira_issue="MAP-1"), 
-    BranchFixture("main"), 
-    BranchFixture(""), 
-    BranchFixture("at3232")
-])
-def branch_fixture(request) -> Generator[BranchFixture, None, None]:
+
+@pytest.fixture(
+    params=[
+        BranchFixture("at-1234-test-branch", valid=True, jira_issue="AT-1234"),
+        BranchFixture("map-1", valid=True, jira_issue="MAP-1"),
+        BranchFixture("main"),
+        BranchFixture(""),
+        BranchFixture("at3232"),
+    ]
+)
+def branch_fixture(request: Any) -> Generator[BranchFixture, None, None]:
+    """
+    Yield a fixture containing details about the branch.
+
+    :param request: pytest fixture containing branch params.
+    :type request: Any
+    :yield: a fixture containing details about the branch.
+    :rtype: Generator[BranchFixture, None, None]
+    """
     yield request.param
 
+
 def test_prepare_commmit_msg(commit_fixture: CommitFixture, branch_fixture: BranchFixture):
+    """
+    Test the prepare_commit_msg script.
+
+    :param commit_fixture: Fixture containing details about the commit message.
+    :type commit_fixture: CommitFixture
+    :param branch_fixture: Fixture containing details about the branch.
+    :type branch_fixture: BranchFixture
+    """
     preparer = CommitMsgPreparer(debug=True)
-    with patch.object(preparer, 'repo') as mock_repo:
+    with patch.object(preparer, "repo") as mock_repo:
         mock_repo.active_branch.name = branch_fixture.name
         mock_repo.remote.return_value.url = "git@gitlab.com:ska-telescope"
         preparer.prepare_msg(commit_fixture.path)
