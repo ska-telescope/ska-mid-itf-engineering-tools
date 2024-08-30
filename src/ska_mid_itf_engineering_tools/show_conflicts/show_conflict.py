eca37549216d9cb1d7f2f53aab6c74b96362a0e5
nnzotho <nnzotho@sarao.ac.za>
2024-08-30 17:40:21 +0200

    AT-2236 work in progress

--- src/ska_mid_itf_engineering_tools/show_conflicts/show_conflict.py
+++ src/ska_mid_itf_engineering_tools/show_conflicts/show_conflict.py
@@ -4,22 +4,35 @@ import gitlab
 import toml
 from time import sleep
 from collections import defaultdict
+import json
 
 def read_toml_to_dict(dictionary, toml_kv)-> dict:
     for k, v in dict(toml_kv).items():
         if (k not in dictionary):
             dictionary[k] = [v]
-            print(f"{k}={v}")
+            #print(f"{k}={v}")
         else:
             dictionary[k].append(v)
-            print(f"****Append {v}")
     return dictionary
 
 def detect_conflicts(dictionary)-> dict:
     confct_dependencies = {}
     for k,v_list in dictionary.items():
-        if (len({}.fromkeys(v_list)) != len(v_list)):
-            confct_dependencies.append(k, v_list)
+        print(f"k={k} ||| v_list={v_list}  type={type(v_list[0])}")
+        if(type(v_list[0]) != str):
+            tmp_lst = []
+            for v in v_list:
+                print(f">>>>>>>>>>>>>>>>>>type={(v['version'])}")
+                tmp_lst += v["version"]
+                print(f"json={v['version']}")
+            confct_dependencies[k] = tmp_lst
+        else:
+             print(f"<<<<<<<<<<<<<<<<< v_list={v_list}")
+             if (len(set(confct_dependencies)) != len(confct_dependencies)): #not unique
+                confct_dependencies[k] = v_list
+             else:
+                 confct_dependencies = {}  
+            
     return confct_dependencies
 
 def main():
@@ -39,8 +52,8 @@ def main():
             read_toml_to_dict(tmp_dict, toml_file["tool"]["poetry"]["group"]["docs"]["dependencies"])
             read_toml_to_dict(tmp_dict, toml_file["tool"]["poetry"]["group"]["dev"]["dependencies"])
             #print(f"{k}={v}")
-            for k,v in tmp_dict.items():
-                print(f"{k},{v}")
+    for module in detect_conflicts(tmp_dict):
+        print(f"$$$$${module}")
 
 if __name__ == "__main__":
     main()
