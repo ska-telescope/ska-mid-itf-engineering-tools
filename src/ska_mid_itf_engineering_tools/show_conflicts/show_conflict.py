b95e5170360c129c75f45a27682546ebb3cf0012
nnzotho <nnzotho@sarao.ac.za>
2024-08-31 22:35:11 +0200

    AT-2236 work in progress 2

--- src/ska_mid_itf_engineering_tools/show_conflicts/show_conflict.py
+++ src/ska_mid_itf_engineering_tools/show_conflicts/show_conflict.py
@@ -1,59 +1,40 @@
-#@dataclass
-#class Repositories:
 import gitlab
 import toml
 from time import sleep
 from collections import defaultdict
 import json
 
-def read_toml_to_dict(dictionary, toml_kv)-> dict:
+def detect_conflicts(dictionary, toml_kv)-> dict:
     for k, v in dict(toml_kv).items():
         if (k not in dictionary):
             dictionary[k] = [v]
-            #print(f"{k}={v}")
         else:
-            dictionary[k].append(v)
+            #Only append unique i.e. different versions of same library
+            if(v not in dictionary[k]):
+                dictionary[k].append(v)
+        print(f"{k}={dictionary[k]}")
     return dictionary
-
-def detect_conflicts(dictionary)-> dict:
-    confct_dependencies = {}
-    for k,v_list in dictionary.items():
-        print(f"k={k} ||| v_list={v_list}  type={type(v_list[0])}")
-        if(type(v_list[0]) != str):
-            tmp_lst = []
-            for v in v_list:
-                print(f">>>>>>>>>>>>>>>>>>type={(v['version'])}")
-                tmp_lst += v["version"]
-                print(f"json={v['version']}")
-            confct_dependencies[k] = tmp_lst
-        else:
-             print(f"<<<<<<<<<<<<<<<<< v_list={v_list}")
-             if (len(set(confct_dependencies)) != len(confct_dependencies)): #not unique
-                confct_dependencies[k] = v_list
-             else:
-                 confct_dependencies = {}  
-            
-    return confct_dependencies
 
 def main():
     gitlab_base_link = "ska-telescope/"
-    repo_list = ["aiv/ska-mid-itf-environments",
-                "ska-mid-itf-engineering-tools"]
+    #"aiv/ska-mid-itf-environments",
+    repo_list = ["ska-mid-itf-engineering-tools",
+                "ska-te-mid-skysimctl"]
 
     gl = gitlab.Gitlab("https://gitlab.com/")
     tmp_dict = {}
     for repo in repo_list:
         project = gl.projects.get(gitlab_base_link + repo)
-        #print(str(project.files.get(file_path='pyproject.toml', ref='main').decode(), "utf-8"))
+        pytoml_file_contents = project.files.get(file_path='pyproject.toml', ref='main').decode().decode()
         print(f"========================{repo}========================")
-        with open("pyproject.toml","r") as f:
-            toml_file = toml.load(f)
-            read_toml_to_dict(tmp_dict, toml_file["tool"]["poetry"]["dependencies"])
-            read_toml_to_dict(tmp_dict, toml_file["tool"]["poetry"]["group"]["docs"]["dependencies"])
-            read_toml_to_dict(tmp_dict, toml_file["tool"]["poetry"]["group"]["dev"]["dependencies"])
-            #print(f"{k}={v}")
-    for module in detect_conflicts(tmp_dict):
-        print(f"$$$$${module}")
+        toml_file = toml.loads(pytoml_file_contents)
+        detect_conflicts(tmp_dict, toml_file["tool"]["poetry"]["dependencies"])
+        detect_conflicts(tmp_dict, toml_file["tool"]["poetry"]["group"]["docs"]["dependencies"])
+        #if("dev" in toml_file):
+        detect_conflicts(tmp_dict, toml_file["tool"]["poetry"]["group"]["dev"]["dependencies"])
+    print("Detected conflicts...")
+    for module, versions_list in tmp_dict.items():
+        print(f"{module}, versions_list={versions_list}")
 
 if __name__ == "__main__":
     main()
