67f7d0c7555eafd29937877dfb0ff08528d18c90
Adriaan de Beer <adebeer42@gmail.com>
2024-10-22 14:15:15 +0200

    AT-2386 linting fixes included

--- tests/unit/tmc_dish_ids/test_instance_is_always_string.py
+++ tests/unit/tmc_dish_ids/test_instance_is_always_string.py
@@ -1,8 +1,8 @@
-"Tests for tmc_config.py."
+"""Tests for tmc_config.py."""
 
-from ska_mid_itf_engineering_tools.tmc_config.tmc_dish_ids import *
+from ska_mid_itf_engineering_tools.tmc_config.tmc_dish_ids import instance
+
 
 def test_instance_is_always_string():
-    "Assert whether or not a string with SKA008 successfully returns as a string."
-    assert instance("SKA008") == '008', "expected \'008\', instead received " + instance("SKA008") 
-    
+    """Assert whether or not a string with SKA008 successfully returns as a string."""
+    assert instance("SKA008") == "008", "expected '008', instead received " + instance("SKA008")
